fee94b40b740b87d2009cc49b354baee49b0c47c
Yan Lagarejo <clagarejo89@gmail.com>
2024-04-24 21:07:57 -0500

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="row mt-5">
-      <div class="col-md-6 ml-3" :disabled>
+      <div class="col-md-6 ml-3">
         <button disabled type="button" class="btn btn-primary py-2 px-5"> Icono </button>
       </div>
       <div class="col-md-6 d-flex justify-content-end">
